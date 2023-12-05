4332fb6784344bc7b3e251eaf955bd257d3b42d2
MsmkAlex <alexcd@msmk.university>
2023-12-05 16:36:09 +0100

    anadidos ficheros

---
+++ Proyecto1/Proyecto1/Proyecto1.cpp
@@ -0,0 +1,12 @@
+﻿// Proyecto1.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Proyecto1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
